48c2ce2c8777e93a286b4d4398cdea1870d3e6e7
gondo <gondo@webdesigners.sk>
2016-10-09 21:55:14 +0100

    Update Resource.php
    Classes with names int, string, float, and bool as well as resource, object, mixed, and numeric are forbidden in PHP 7.
    http://php.net/manual/en/reserved.other-reserved-words.php

--- src/Types/Resource.php
+++ src/Types/Resource.php
@@ -17,7 +17,7 @@ use phpDocumentor\Reflection\Type;
 /**
  * Value Object representing the 'resource' Type.
  */
-final class Resource implements Type
+final class Resource_ implements Type
 {
     /**
      * Returns a rendered output of the Type as it would be used in a DocBlock.
