b6a886bbc70d7db0c84d34281114a72a0a11fe73
smoench <simon.moench@sensiolabs.de>
2020-07-16 13:00:43 +0200

    support psalm scalar types

--- src/PseudoTypes/LowercaseString.php
+++ src/PseudoTypes/LowercaseString.php
@@ -11,22 +11,29 @@ declare(strict_types=1);
  * @link      http://phpdoc.org
  */
 
-namespace phpDocumentor\Reflection\Types;
+namespace phpDocumentor\Reflection\PseudoTypes;
 
+use phpDocumentor\Reflection\PseudoType;
 use phpDocumentor\Reflection\Type;
+use phpDocumentor\Reflection\Types\String_;
 
 /**
  * Value Object representing the type 'string'.
  *
  * @psalm-immutable
  */
-class String_ implements Type
+final class LowercaseString extends String_ implements PseudoType
 {
+    public function underlyingType(): Type
+    {
+        return new String_();
+    }
+
     /**
      * Returns a rendered output of the Type as it would be used in a DocBlock.
      */
     public function __toString() : string
     {
-        return 'string';
+        return 'lowercase-string';
     }
 }
