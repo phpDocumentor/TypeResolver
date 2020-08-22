316ba16d4e93f4a9e6163ab1e39ae4fb4f374a8a
Mike van Riel <me@mikevanriel.com>
2020-08-22 11:40:19 +0200

    Add test for Boolean type too

---
+++ tests/unit/Types/BooleanTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of phpDocumentor.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @link      http://phpdoc.org
+ */
+
+namespace phpDocumentor\Reflection\Types;
+
+use phpDocumentor\Reflection\Types\Boolean;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \phpDocumentor\Reflection\Types\Boolean
+ */
+final class BooleanTest extends TestCase
+{
+    /**
+     * @covers ::__toString
+     */
+    public function testBooleanStringifyCorrectly() : void
+    {
+        $type = new Boolean();
+
+        $this->assertSame('bool', (string) $type);
+    }
+}
