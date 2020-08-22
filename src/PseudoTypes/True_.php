229d114d8450af1e0e5a3b80aecdd5cb023c07df
Mike van Riel <me@mikevanriel.com>
2020-08-22 11:04:12 +0200

    Fix linting issues

--- src/PseudoTypes/True_.php
+++ src/PseudoTypes/True_.php
@@ -2,12 +2,24 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of phpDocumentor.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @link      http://phpdoc.org
+ */
+
 namespace phpDocumentor\Reflection\PseudoTypes;
 
 use phpDocumentor\Reflection\PseudoType;
 use phpDocumentor\Reflection\Type;
 use phpDocumentor\Reflection\Types\Boolean;
 
+/**
+ * Value Object representing the PseudoType 'False', which is a Boolean type.
+ */
 final class True_ extends Boolean implements PseudoType
 {
     public function underlyingType() : Type
