069ab05252275a9adaf623601589e7a98c048a02
Mike van Riel <me@mikevanriel.com>
2020-08-25 07:50:39 +0200

    Fix styling issues

--- src/PseudoTypes/True_.php
+++ src/PseudoTypes/True_.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @link http://phpdoc.org
+ * @link https://phpdoc.org
  */
 
 namespace phpDocumentor\Reflection\PseudoTypes;
@@ -16,6 +16,7 @@ namespace phpDocumentor\Reflection\PseudoTypes;
 use phpDocumentor\Reflection\PseudoType;
 use phpDocumentor\Reflection\Type;
 use phpDocumentor\Reflection\Types\Boolean;
+use function class_alias;
 
 /**
  * Value Object representing the PseudoType 'False', which is a Boolean type.
