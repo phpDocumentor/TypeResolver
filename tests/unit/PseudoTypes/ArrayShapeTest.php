7aae003c8defee5362b1c8a96b2609190187ad7f
Antoine Lamé <antoine.lame@gmail.com>
2023-11-24 17:20:58 +0100

    Add ArrayShapeTest

---
+++ tests/unit/PseudoTypes/ArrayShapeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace phpDocumentor\Reflection\PseudoTypes;
+
+use phpDocumentor\Reflection\PseudoTypes\ArrayShape;
+use phpDocumentor\Reflection\PseudoTypes\ArrayShapeItem;
+use PHPUnit\Framework\TestCase;
+
+class ArrayShapeTest extends TestCase
+{
+    /**
+     * @covers ::getItems
+     */
+    public function testExposeItems(): void
+    {
+        $item1 = new ArrayShapeItem('foo', new True_(), false);
+        $item2 = new ArrayShapeItem('bar', new False_(), true);
+
+        $arrayShape = new ArrayShape($item1, $item2);
+
+        $this->assertSame([$item1, $item2], $arrayShape->getItems());
+    }
+}
