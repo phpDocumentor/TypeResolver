464e44e55532b1b2aa103ba94beed167a0da1528
Danny van der Sluijs <danny.vandersluijs@icloud.com>
2025-04-20 20:30:16 +0200

    Correct documentation reference
    In https://github.com/phpDocumentor/TypeResolver/commit/d415ee87513a4c304c6e0fe33011c86f6427e568 the method `createFromClassReflector` was renamed to `createFromReflector`

--- src/Types/Context.php
+++ src/Types/Context.php
@@ -28,7 +28,7 @@ use function trim;
  * own application it is possible to generate a Context class using the ContextFactory; this will analyze the file in
  * which an associated class resides for its namespace and imports.
  *
- * @see ContextFactory::createFromClassReflector()
+ * @see ContextFactory::createFromReflector()
  * @see ContextFactory::createForNamespace()
  *
  * @psalm-immutable
