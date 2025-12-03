6220be82d8def391d157dffbeafe7b3b795f3ecb
Максим Спирков <spirkov.2001@mail.ru>
2025-12-03 12:15:04 +0400

    Refactor the `Scalar` type

--- src/PseudoTypes/Scalar.php
+++ src/PseudoTypes/Scalar.php
@@ -11,17 +11,28 @@ declare(strict_types=1);
  * @link      http://phpdoc.org
  */
 
-namespace phpDocumentor\Reflection\Types;
+namespace phpDocumentor\Reflection\PseudoTypes;
 
+use phpDocumentor\Reflection\PseudoType;
 use phpDocumentor\Reflection\Type;
+use phpDocumentor\Reflection\Types\Boolean;
+use phpDocumentor\Reflection\Types\Compound;
+use phpDocumentor\Reflection\Types\Float_;
+use phpDocumentor\Reflection\Types\Integer;
+use phpDocumentor\Reflection\Types\String_;
 
 /**
  * Value Object representing the 'scalar' pseudo-type, which is either a string, integer, float or boolean.
  *
  * @psalm-immutable
  */
-final class Scalar implements Type
+final class Scalar implements PseudoType
 {
+    public function underlyingType(): Type
+    {
+        return new Compound([new String_(), new Integer(), new Float_(), new Boolean()]);
+    }
+
     /**
      * Returns a rendered output of the Type as it would be used in a DocBlock.
      */
